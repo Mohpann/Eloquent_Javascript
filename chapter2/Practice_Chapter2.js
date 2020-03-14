0b762ceb91e2cefaf4512adf2cc3171c16e4ef75
jopann <jopann@gmail.com>
2020-03-14 20:01:28 +0000

    hw

--- chapter2/Practice_Chapter2.js
+++ chapter2/Practice_Chapter2.js
@@ -1,2 +1,8 @@
-let test = "yes"
-console.log(test)
+let test = "Hello there";
+console.log(test);
+
+let hashtag = "#"
+for(let counter = 0; counter < 7; counter = counter + 1)    {
+    console.log(hashtag);
+    hashtag = hashtag + "#";
+}
